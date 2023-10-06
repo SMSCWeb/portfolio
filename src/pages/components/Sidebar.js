3b8b3d5091e6f12395b6985c1243c0369174b92d
ShekharHans <shekharhans161@gmail.com>
2023-10-07 03:08:10 +0530

    add animation

--- src/pages/components/Sidebar.js
+++ src/pages/components/Sidebar.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInstagram, faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { Slide, Fade } from "react-awesome-reveal";
+
 const Sidebar = () => {
     const [menuVisible, setMenuVisible] = useState(false);
 
@@ -29,46 +31,52 @@ const Sidebar = () => {
                         </div>
                     )}
                     {menuVisible && (
-                        <div className="menu-holder">
-                            <div className={`menu-inside ${menuVisible ? 'open' : ''}`}>
-                                <div className="menu-nav-container">
-                                    <ul className="internal-nav-links">
-                                        <li><a href="#1"><span>Projects</span></a>
-                                            <img src="https://images.unsplash.com/photo-1615713170963-2595d2c721bb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=934&amp;q=80" />
-                                        </li>
-                                        <li><a href="#2"><span>Recognition</span></a>
-                                            <img src="https://images.unsplash.com/photo-1534120247760-c44c3e4a62f1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=2148&amp;q=80" />
-                                        </li>
-                                        <li><a href="#3"><span>Studio</span></a>
-                                            <img src="https://images.unsplash.com/photo-1554941829-202a0b2403b8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=2100&amp;q=80" />
-                                        </li>
-                                        <li><a href="#4"><span>Contribution</span></a>
-                                            <img src="https://images.unsplash.com/photo-1593697821028-7cc59cfd7399?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2100&amp;q=80" />
-                                        </li>
-                                        <li><a href="#5"><span>Career</span></a>
-                                            <img src="https://images.unsplash.com/photo-1588200618450-3a5b1d3b9aa5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=2100&amp;q=80" />
-                                        </li>
-                                    </ul>
-                                    <ul className="external-nav-links">
-                                        <li>
-                                            <a href="www.facebook.com">
-                                                <FontAwesomeIcon icon={faFacebook} className='icon' />
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="www.instagram.com" className='icon'>
-                                                <FontAwesomeIcon icon={faInstagram} />
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="www.youtube.com" className='icon'>
-                                                <FontAwesomeIcon icon={faLinkedin} />
-                                            </a>
-                                        </li>
-                                    </ul>
+                        <Slide duration={2000}>
+                            <div className="menu-holder">
+                                <div className={`menu-inside ${menuVisible ? 'open' : ''}`}>
+                                    <div className="menu-nav-container">
+                                        <ul className="internal-nav-links">
+                                            <Slide cascade>
+                                                <li><a href="#1"><span>Projects</span></a>
+                                                    <img src="https://images.unsplash.com/photo-1615713170963-2595d2c721bb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=934&amp;q=80" />
+                                                </li>
+                                                <li><a href="#2"><span>Recognition</span></a>
+                                                    <img src="https://images.unsplash.com/photo-1534120247760-c44c3e4a62f1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=2148&amp;q=80" />
+                                                </li>
+                                                <li><a href="#3"><span>Studio</span></a>
+                                                    <img src="https://images.unsplash.com/photo-1554941829-202a0b2403b8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=2100&amp;q=80" />
+                                                </li>
+                                                <li><a href="#4"><span>Contribution</span></a>
+                                                    <img src="https://images.unsplash.com/photo-1593697821028-7cc59cfd7399?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2100&amp;q=80" />
+                                                </li>
+                                                <li><a href="#5"><span>Career</span></a>
+                                                    <img src="https://images.unsplash.com/photo-1588200618450-3a5b1d3b9aa5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=2100&amp;q=80" />
+                                                </li>
+                                            </Slide>
+                                        </ul>
+                                        <ul className="external-nav-links">
+                                            <Fade cascade duration={2000} delay={4000}>
+                                                <li>
+                                                    <a href="www.facebook.com">
+                                                        <FontAwesomeIcon icon={faFacebook} className='icon' />
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="www.instagram.com" className='icon'>
+                                                        <FontAwesomeIcon icon={faInstagram} />
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="www.youtube.com" className='icon'>
+                                                        <FontAwesomeIcon icon={faLinkedin} />
+                                                    </a>
+                                                </li>
+                                            </Fade>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        </Slide>
                     )}
                 </div>
             </div>
